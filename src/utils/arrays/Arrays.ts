27de0aecafa55e6b6a07958d2f8de986ab996c1a
I. Kapri <my@mail.com>
2023-04-22 19:11:26 +0200

    U-0002: aSx8Sau76mE1

--- src/utils/arrays/Arrays.ts
+++ src/utils/arrays/Arrays.ts
@@ -1,7 +1,7 @@
 export abstract class Arrays {
-  static first<T>(value: Array<T>): T | null;
-  static first<T>(value: ReadonlyArray<T>): T | null;
-  static first<T>(value: Array<T> | ReadonlyArray<T>): T | null {
+  static first<T>(value: T[]): T | null;
+  static first<T>(value: readonly T[]): T | null;
+  static first<T>(value: T[] | readonly T[]): T | null {
     if (Arrays.isEmpty(value)) {
       return null;
     }
@@ -9,52 +9,63 @@ export abstract class Arrays {
     return value[0];
   }
 
-  static has<T>(array: Array<T>, item: T): boolean;
-  static has<T>(array: ReadonlyArray<T>, item: T): boolean;
-  static has<T>(array: Array<T> | ReadonlyArray<T>, item: T): boolean {
+  static has<T>(array: T[], item: T): boolean;
+  static has<T>(array: readonly T[], item: T): boolean;
+  static has<T>(array: T[] | readonly T[], item: T): boolean {
     return array.includes(item);
   }
 
-  static insertAfter<T>(array: Array<T>, item: T): Array<T>;
-  static insertAfter<T>(array: ReadonlyArray<T>, item: T): ReadonlyArray<T>;
-  static insertAfter<T>(array: Array<T> | ReadonlyArray<T>, item: T): Array<T> | ReadonlyArray<T> {
-    const itemIndex = array.findIndex((value) => value === item);
-    if (itemIndex !== -1 && array.length > itemIndex) {
-    }
-
-    return array.filter((v) => v !== item);
+  static insertAt<T>(array: T[], index: number, item: T): T[];
+  static insertAt<T>(array: T[], index: number, item: T[]): T[];
+  static insertAt<T>(array: T[], index: number, item: T | T[]): T[] {
+    item = Arrays.isArray(item) ? item : [item];
+    return [...array.slice(0, index), ...item, ...array.slice(index)]
   }
 
-  static isArray<T>(
-    value?: any | Array<T> | ReadonlyArray<T>
-  ): value is Array<T> | ReadonlyArray<T> {
+  // adpoted from https://mariusschulz.com/blog/read-only-array-and-tuple-types-in-typescript
+  static intersperse<T>(array: T[], separator: T): T[] {
+    const result: T[] = [];
+    for (let i = 0; i < array.length; i++) {
+      if (i !== 0) {
+        result.push(separator);
+      }
+      result.push(array[i]);
+    }
+    return result;
+  }
+
+  static isArray(value?: any | any[]): value is any[] {
     return Object.prototype.toString.call(value) === '[object Array]';
   }
 
-  static isEmpty<T>(value: Array<T>): boolean;
-  static isEmpty<T>(value: ReadonlyArray<T>): boolean;
-  static isEmpty<T>(value: Array<T> | ReadonlyArray<T>): boolean {
+  static isEmpty<T>(value: T[]): boolean;
+  static isEmpty<T>(value: readonly T[]): boolean;
+  static isEmpty<T>(value: T[] | readonly T[]): boolean {
     return !value.length;
   }
 
-  static last<T>(value: Array<T>): T | null
-  static last<T>(value: ReadonlyArray<T>): T | null
-  static last<T>(value: Array<T> | ReadonlyArray<T>): T | null {
+  static isReadonlyArray(value?: any | readonly any[]): value is readonly any[] {
+    return Arrays.isArray(value);
+  }
+
+  static last<T>(value: T[]): T | null
+  static last<T>(value: readonly T[]): T | null
+  static last<T>(value: T[] | readonly T[]): T | null {
     if (Arrays.isEmpty(value)) {
       return null;
     }
     return value[value.length - 1];
   }
 
-  static removeAll<T>(array: Array<T>, item: T): Array<T>;
-  static removeAll<T>(array: ReadonlyArray<T>, item: T): ReadonlyArray<T>;
-  static removeAll<T>(array: Array<T> | ReadonlyArray<T>, item: T): Array<T> | ReadonlyArray<T> {
+  static removeAll<T>(array: T[], item: T): T[];
+  static removeAll<T>(array: readonly T[], item: T): readonly T[];
+  static removeAll<T>(array: T[] | readonly T[], item: T): T[] | readonly T[] {
     return array.filter((v) => v !== item);
   }
 
-  static unique<T>(array: Array<T>): Array<T>;
-  static unique<T>(array: ReadonlyArray<T>): ReadonlyArray<T>;
-  static unique<T>(array: Array<T> | ReadonlyArray<T>): Array<T> | ReadonlyArray<T> {
+  static unique<T>(array: T[]): T[];
+  static unique<T>(array: readonly T[]): readonly T[];
+  static unique<T>(array: T[] | readonly T[]): T[] | readonly T[] {
     return array.filter((item, index, self) => self.indexOf(item) === index);
   }
 }
